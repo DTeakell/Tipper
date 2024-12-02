1064eb7bfb9251f583568036d44675ece46ff154
Dillon Teakell <teakelld@icloud.com>
2024-12-01 19:02:20 -0500

    Initial Commit

---
+++ Tipper/TipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipperApp.swift
+//  Tipper
+//
+//  Created by Dillon Teakell on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
